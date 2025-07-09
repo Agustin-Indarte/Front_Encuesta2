153e8ecf54b00a125fc98f962f25431f3d3b9fe3
Agustin Indarte <aindarte111@gmail.com>
2025-07-08 21:35:26 -0300

    Estructuracion base de la page AdmHome con el navbar, titulo, select de estado,categoria y btn para enviar la encuesta

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export {default as Form_Login} from './Login/Form_Login'
 export {default as Form_Register} from './Register/Form_Register'
+export {default as Navbar } from './Navbar/Navbar'
--- src/pages/Admin_Encuestas/Admin_Encuestas.jsx
+++ src/pages/Admin_Encuestas/Admin_Encuestas.jsx
@@ -1,8 +1,77 @@
 import React from 'react'
-
+import Navbar from '../../components/Navbar/Navbar'
+import { Row, Form, Col,Button } from 'react-bootstrap'
 function Admin_Encuestas() {
   return (
-    <div>Admin_Encuestas</div>
+    <>
+      <Navbar />
+
+      <div className="Container-Page">
+        <Row className="w-100">
+          <Col md={3}>
+            <h2 className='fw-bold text-primary'>NUEVA ENCUESTA</h2>
+          </Col>
+          <Col md={3}>
+            <Form.Group className="mb-2">
+              <Form.Control
+                type="email"
+                placeholder="Nombre"
+              />
+            </Form.Group>
+          </Col>
+
+          <Col md={3}>
+            <Form.Select
+              name="categoria"
+              className="InputEncuesta"
+            >
+              <option value="">Seleccionar Estado</option>
+              <option value="">Activa</option>
+              <option value="">Inactiva</option>
+            </Form.Select>
+          </Col>
+
+          <Col md={3}>
+            <Form.Select
+              name="categoria"
+              className="InputEncuesta"
+            >
+              <option value="">Seleccionar Categoria</option>
+              <option value="">Deportes</option>
+              <option value="">Salud</option>
+
+            </Form.Select>
+          </Col>
+
+          
+        </Row>
+
+         <Row className="w-100">
+         
+        <div style={{height:"480px"}}>
+
+        </div>
+          
+        </Row>
+
+         <Row className="w-100">
+          <Col md={9}>
+           
+          </Col>
+         
+
+          <Col md={3}>
+            <Button className='w-100 fs-4 fw-bold'>
+                Publicar
+            </Button>
+          </Col>
+
+          
+        </Row>
+
+        
+      </div>
+    </>
   )
 }
 
