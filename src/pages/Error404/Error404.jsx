b772508e3441fa29e6b202dc85dcd7217ad1378f
soledad <mariasoledadcaria@gmail.com>
2025-08-20 00:08:31 -0300

    error404

--- src/pages/Error404/Error404.jsx
+++ src/pages/Error404/Error404.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
 
-function Error404() {
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./Error404.css";
+import errorImg from "../../assets/error1.png"; 
+
+const Error404 = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>Error404</div>
-  )
-}
+    <div className="error-container">
+      <img src={errorImg} alt="Error 404 - Encuesta2" className="error-image" />
+     
+      <p className="error-text">
+        Lo sentimos, la página que buscas no existe en <strong>Encuesta2</strong>.
+      </p>
+      <button className="btn-back" onClick={() => navigate("/")}>
+        Volver al Inicio
+      </button>
+    </div>
+  );
+};
 
-export default Error404
+export default Error404;
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,4 +5,4 @@ export {default as User_Encuestas} from './User_Encuestas/User_Encuestas';
 export {default as User_Home} from './User_Home/User_Home';
 export {default as Admin_Encuestas} from './Admin_Encuestas/Admin_Encuestas';
 export {default as Admin_Home} from './Admin_Home/Admin_Home';
-export {default as Error404} from './Error404/Error404'
+export { default as Error404 } from "./Error404/Error404";
