521ee4747793e0d545e0c1361e9081d287f55be8
Agustin Indarte <aindarte111@gmail.com>
2025-08-20 16:59:01 -0300

    Correcion de importacion de estilos

--- src/components/Admin_Encuestas/Card/AdmCard.jsx
+++ src/components/Admin_Encuestas/Card/AdmCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Card } from 'react-bootstrap';
-import './admCard.css'
+import './AdmCard.css'
 import { TypeText, TypeQuestion, TypeMultimedia } from '../../../components'
 
 const AdmCard = ({ card, isActive, onUpdateContent }) => {
